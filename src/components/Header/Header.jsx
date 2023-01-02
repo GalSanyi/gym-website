f4d4e07fd8f9b63bf8a72ad078e91c40271af55a
GalSanyi <86233181+GalSanyi@users.noreply.github.com>
2023-01-02 23:43:42 +0100

    navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import '../../styles/header.css';
 import logo from '../../assets/img/dumble.png';
+import {BsList} from 'react-icons/bs';
+import { v4 as uuidv4 } from 'uuid';
+const nav__link = [
+  {
+    path: '#',
+    display: 'Home',
+  },
+  {
+    path: '#',
+    display: 'schedule',
+  },
+  {
+    path: '#',
+    display: 'Classes',
+  },
+  {
+    path: '#',
+    display: 'Pricing',
+  },
+];
 const Header = () => {
   return (
-    <header>
+    <header className='header'>
       <div className="container">
         <div className="nav__wrapper">
           {/* logo */}
@@ -14,6 +34,21 @@ const Header = () => {
             <h2>FitBody</h2>
           </div>
           {/* navigation menu */}
+          <div className="navigation">
+            <ul className="menu">
+              {nav__link.map(item => (
+                <li key={uuidv4()} className="nav__item">
+                  <a href={item.path}>{item.display}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          {/* nav right */}
+          <div className='nav-right'>
+            <button className='register__btn'>Register</button>
+            <span className='mobile__menu'><BsList/></span>
+          </div>
         </div>
       </div>
     </header>
